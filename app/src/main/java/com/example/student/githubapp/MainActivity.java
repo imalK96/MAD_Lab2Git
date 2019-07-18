c21a6a3b8abf2ae03c6f04b6a592bbbce2f275ad
imalK96 <52731064+imalK96@users.noreply.github.com>
2019-07-18 12:51:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/student/githubapp/MainActivity.java
+++ app/src/main/java/com/example/student/githubapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        system.out.println("ABC");
+        System.out.println("ABC");
     }
 }
